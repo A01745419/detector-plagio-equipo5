d79a3a233184be2e7d23706df782028c693d820b
Jose Luis Madrigal Sanchez <A01745419@itesm.mx>
2024-05-22 13:53:51 -0600

    Organización por funciones
    Funciones nombradas como en el diseño de la propuesta. Por ahora se hace con párrafos en variables, por lo que falta incluir lectura de archivo.

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,7 @@
+# José Luis Madrigal Sánchez A01745419
+# Paulo Ogando Gulías A01751587
+# César Emiliano Palome Luna A01746493
+# Creado 21/05/2024
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
@@ -10,56 +14,53 @@ nltk.download('punkt')
 
 # Primer párrafo
 parrafo1 = """La historia detrás de los orígenes de Batman es casi tan interesante en la vida real como en los cómics. El Hombre Murciélago nace de una idea original de Bob Kane, que tenía poco más que un nombre, y su colaborador habitual Bill Finger, que diseñó tanto el traje como la historia de Bruce Wayne. Un nombre que, por cierto, no es para nada casual."""
-# Párrafo similar que habla del mismo tema
-parrafo2 ="""La historia de cómo se creó el personaje de Batman es una de las más fascinantes de los cómics. Originalmente creado por Bob Kane de forma muy somera lo que después trajo polémica por su autoría, no fue hasta que su colaborador Bill Finger llegó y desarrolló el concepto completo de lo que hoy entendemos como el personaje. Una de las contribuciones más significativas al mito de Batman hecha por Finger fue el propio nombre del personaje, Bruce Wayne."""
 # Primer párrafo parafraseado (plagio)
 parafraseado1 = """La historia del origen de Batman es casi tan interesante en la vida real como lo es en los cómics. Batman nació de la idea original de Bob Kane, que tiene un solo nombre, y su frecuente colaborador Bill Finger, quien diseñó el traje de Batman y la historia de Bruce Wayne. Por cierto, este nombre no es casual."""
 
+# Remover puntuación
+def limpieza(texto):
+    return re.sub(r'[^\w\s]', '', texto)
 
-def remove_punctuation(text):
-    return re.sub(r'[^\w\s]', '', text)
+# Párrafos sin puntuación
+parrafo_limpio1 = limpieza(parrafo1)
+parrafo_limpio2 = limpieza(parafraseado1)
 
-parrafo_limpio1 = remove_punctuation(parrafo1)
-parrafo_limpio2 = remove_punctuation(parrafo2)
-parrafo_limpio3 = remove_punctuation(parafraseado1)
-
-# Utilizacion de Stemming
-def stem_sentence(sentence):
+# Utilización de Stemming para preprocesamiento
+def stemming(oraciones):
     ps = PorterStemmer()
-    palabras = word_tokenize(sentence)
+    palabras = word_tokenize(oraciones)
     stemmed_tokens = [ps.stem(palabra) for palabra in palabras]
     return ' '.join(stemmed_tokens)
 
-# Aplicar stemming
-stemmed_sentence1 = stem_sentence(parrafo_limpio1)
-stemmed_sentence2 = stem_sentence(parrafo_limpio2)
-stemmed_sentence3 = stem_sentence(parrafo_limpio3)
+# Parrafos preprocesados
+stemmed_oraciones1 = stemming(parrafo_limpio1)
+stemmed_oraciones2 = stemming(parrafo_limpio2)
 
-# Ngramas y similitud
-def calculate_similarity(tokens1, tokens2, n):
+# Obtención de vectores por medio de tokenización
+def vectorizacion(tokens1, tokens2, n):
     vectorizer = CountVectorizer(analyzer='word', ngram_range=(n, n))
-    ngrams = vectorizer.fit_transform([tokens1, tokens2])
-    print(f"Corpus {n}grama: {vectorizer.get_feature_names_out()}")
-    print("------------------------------------")
-    print(f"Vector {n}grama: {ngrams.toarray()}")
-    print("------------------------------------")
-    similarity = cosine_similarity(ngrams)[0, 1]
-    return similarity
+    ngramas = vectorizer.fit_transform([tokens1, tokens2])
+    # print(f"Corpus {n}grama: {vectorizer.get_feature_names_out()}")
+    # print("------------------------------------")
+    # print(f"Vector {n}grama: {ngrams.toarray()}")
+    # print("------------------------------------")
+    return ngramas
 
-# Similitudes entre párrafos con tema similar
-unigram_similarity = calculate_similarity(stemmed_sentence1, stemmed_sentence2, 1)
-bigram_similarity = calculate_similarity(stemmed_sentence1, stemmed_sentence2, 2)
-trigram_similarity = calculate_similarity(stemmed_sentence1, stemmed_sentence2, 3)
+# Cálculo de similitud por coseno
+def calcular_similitud(ngramas):
+    similitud = cosine_similarity(ngramas)[0, 1]
+    return similitud
 
-# Similitudes entre original y parafraseado
-unigram_similarity_paraphrasing = calculate_similarity(stemmed_sentence1, stemmed_sentence3, 1)
-bigram_similarity_paraphrasing = calculate_similarity(stemmed_sentence1, stemmed_sentence3, 2)
-trigram_similarity_paraphrasing = calculate_similarity(stemmed_sentence1, stemmed_sentence3, 3)
+# Vectores
+vector_unigrama = vectorizacion(stemmed_oraciones1, stemmed_oraciones2, 1)
+vector_bigrama = vectorizacion(stemmed_oraciones1, stemmed_oraciones2, 2)
+vector_trigrama = vectorizacion(stemmed_oraciones1, stemmed_oraciones2, 3)
 
-print(f"Unigram similarity between paragraphs with same topic: {unigram_similarity}")
-print(f"Bigram similarity between paragraphs with same topic: {bigram_similarity}")
-print(f"Trigram similarity between paragraphs with same topic: {trigram_similarity}")
+# Similitudes
+similitud_unigrama = calcular_similitud(vector_unigrama)
+similitud_bigrama = calcular_similitud(vector_bigrama)
+similitud_trigrama = calcular_similitud(vector_trigrama)
 
-print(f"Unigram similarity between original and paraphrased: {unigram_similarity_paraphrasing}")
-print(f"Bigram similarity between original and paraphrased: {bigram_similarity_paraphrasing}")
-print(f"Trigram similarity between original and paraphrased: {trigram_similarity_paraphrasing}")
+print(f"Porcentaje de similitud por unigrama: {similitud_unigrama * 100} %")
+print(f"Porcentaje de similitud por bigrama: {similitud_bigrama * 100} %")
+print(f"Porcentaje de similitud por trigrama: {similitud_trigrama * 100} %")
