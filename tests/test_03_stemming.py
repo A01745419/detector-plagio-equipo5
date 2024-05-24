946ae5c356294fa9ef3da72443cfb640fe880fb6
Paulo Ogando Gulias <paulo.ogando2000@gmail.com>
2024-05-24 17:16:40 -0600

    Added 2 tests

--- tests/test_03_stemming.py
+++ tests/test_03_stemming.py
@@ -9,9 +9,25 @@ class TestStemming(TestCase):
         self.pc = PlagiarismChecker()
 
     def test_stemming(self):
-        texto = 'This is a test for the plagiarism checker.\n'
+        texto = 'This is a test for the plagiarism checker.'
         texto_limpio = self.pc.limpieza(texto)
         texto_stemming = self.pc.stemming(texto_limpio)
         self.assertEqual(
             texto_stemming,
             'thi is a test for the plagiar checker')
+
+    def test_stemming_with_2_sentences(self):
+        texto = 'This is a test for the plagiarism checker. This is a second sentence.'
+        texto_limpio = self.pc.limpieza(texto)
+        texto_stemming = self.pc.stemming(texto_limpio)
+        self.assertEqual(
+            texto_stemming,
+            'thi is a test for the plagiar checker thi is a second sentenc')
+
+    def test_stemming_with_3_sentences(self):
+        texto = 'This is a test for the plagiarism checker. This is a second sentence.\n This is a third sentence.'
+        texto_limpio = self.pc.limpieza(texto)
+        texto_stemming = self.pc.stemming(texto_limpio)
+        self.assertEqual(
+            texto_stemming,
+            'thi is a test for the plagiar checker thi is a second sentenc thi is a third sentenc')
